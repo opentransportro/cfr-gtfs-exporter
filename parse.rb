aac7c26f845ffc33c932ce212cc8f75c3423d125
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2016-12-23 13:33:23 +0100

    Timetables 2016-2017 $ ruby parse.rb

--- parse.rb
+++ parse.rb
@@ -2,7 +2,7 @@ APP_PATH = File.expand_path(File.dirname(__FILE__))
 
 require "#{APP_PATH}/gtfs.rb"
 
-timetables_start_date = '2015-12-13'
-timetables_end_date = '2016-12-10'
+timetables_start_date = '2016-12-11'
+timetables_end_date = '2017-12-09'
 
 GovRoGTFSConverter::parse_and_save_for_dates(timetables_start_date, timetables_end_date)
