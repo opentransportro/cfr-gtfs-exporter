7788657e8825b8468d4116ad4b82f30cfcfbe350
Vasile Cotovanu <vasile.cotovanu@gmail.com>
2018-12-23 00:09:24 +0100

    Adds new dataset config

--- parse.rb
+++ parse.rb
@@ -2,7 +2,7 @@ APP_PATH = File.expand_path(File.dirname(__FILE__))
 
 require "#{APP_PATH}/gtfs.rb"
 
-timetables_start_date = '2017-12-12'
-timetables_end_date = '2018-12-08'
+timetables_start_date = '2018-12-09'
+timetables_end_date = '2019-12-14'
 
 GovRoGTFSConverter::parse_and_save_for_dates(timetables_start_date, timetables_end_date)
