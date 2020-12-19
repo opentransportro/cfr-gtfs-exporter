5b357cbcf26b793d8a358c22177f06fd467362d1
Vlad Vesa <hello@vladvesa.ro>
2020-12-19 08:15:51 +0200

    add colors to routes

--- gtfs.rb
+++ gtfs.rb
@@ -382,6 +382,8 @@ class GovRoGTFSConverter
                     'agency_id' => trip_data['agency_id'],
                     'route_short_name' => "#{trip_data['trip_short_name']}: #{first_stop['stop_name']} - #{last_stop['stop_name']}",
                     'route_long_name' => "#{trip_data['trip_short_name']}: #{stop_names.join(' - ')}",
+                    'route_color' => 'FF5B33',
+                    'route_text_color' => '000000',
                     # https://developers.google.com/transit/gtfs/reference#routestxt
                     'route_type' => '2',
                 }
